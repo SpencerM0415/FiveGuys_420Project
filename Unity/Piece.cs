ed60433e987ab7e0091bf095ce750b83a6c54469
SpencerM0415 <76704737+SpencerM0415@users.noreply.github.com>
2021-04-11 19:24:23 -0400

    Add files via upload
    Small script that will hold attributes for game pieces

---
+++ Unity/Piece.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartoGame
+{   
+	public Piece()
+	{
+		public string height = "";
+		public string color = "";
+		public string hole = "";
+		public string type = "";
+		public char pieceSign = "";
+	}
+}
