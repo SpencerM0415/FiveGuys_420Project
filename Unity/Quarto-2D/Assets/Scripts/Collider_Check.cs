86c97b625e1150444bc3cd26797ddf5d64d976b5
AustinL0214 <Bigfatcock69>
2021-04-11 15:57:14 -0400

    added Menu scene and scene manager. improved drag and place routines. 1 small bug not letting me pick up piece after on board.

---
+++ Unity/Quarto-2D/Assets/Scripts/Collider_Check.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider_Check : MonoBehaviour
+{
+    Box box;
+    //public CircleCollider2D spaceCollider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+//        if (box.isBeingHeld)
+//        {
+            Debug.Log("Held");
+            //Destroy(this.gameObject);
+//        }
+        //transform.position = startPoint.transform.position;
+    }
+
+}
--- Unity/Quarto-2D/Assets/Scripts/PieceMove.cs
+++ Unity/Quarto-2D/Assets/Scripts/PieceMove.cs
@@ -7,6 +7,7 @@ public class PieceMove : MonoBehaviour
     public Transform[] waypoints; //waypoints to store board locations
     public Transform startPoint;
     public Box box;
+    private bool onBoard = false;
 
     [SerializeField] 
 
@@ -19,21 +20,30 @@ public class PieceMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if ((box.isBeingHeld == false) && (onBoard == false))
+        {
+            transform.position = startPoint.transform.position;
+        }
     }
 
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        //        if (box.isBeingHeld)
-        //        {
-        Debug.Log("Held");
-        //Destroy(this.gameObject);
-        //        }
+        onBoard = true;
         if ((box.isBeingHeld == false) && (other.tag == "Point"))
         {
-            this.transform.position = other.transform.position;
+            transform.position = other.transform.position;
+        } 
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Point")
+        {
+            onBoard = false;
         }
     }
+
 
 }
